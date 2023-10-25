e8e50bfe34cb429e4559044378524d02d7f9dbf6
ShmsErl <erol.sehmus1@gmail.com>
2023-10-25 20:52:40 +0300

    Order Details nesnesini Constructor ve Getter Setter metodları tanımlandı

--- src/main/java/org/example/Model/OrderDetail.java
+++ src/main/java/org/example/Model/OrderDetail.java
@@ -2,8 +2,31 @@ package org.example.Model;
 
 public class OrderDetail {
 
+
     private int id;
     private int orderShipss;
 
+    public OrderDetail() {
+    }
 
+    public OrderDetail(int id, int orderShipss) {
+        this.id = id;
+        this.orderShipss = orderShipss;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderShipss() {
+        return orderShipss;
+    }
+
+    public void setOrderShipss(int orderShipss) {
+        this.orderShipss = orderShipss;
+    }
 }
